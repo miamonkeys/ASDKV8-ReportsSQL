17c1d681a517bf7deb5c7ce861e4c22c8ae9aa00
miamonkeys <miamonkeys@protonmail.com>
2022-02-20 19:56:38 -0500

    Se borra la unión con vista ALL_CATEGORY.

--- INCI_REABIERTOS.sql
+++ INCI_REABIERTOS.sql
@@ -3,7 +3,6 @@ SELECT
 	T3.inci_id_by_project AS 'ID',
 	T3.inci_composed_id AS 'ID COMPUESTO',
 	CONVERT(VARCHAR, T3.inci_opened_date, 120) AS 'FECHA DE APERTURA',
-	T5.CONCAT_CAT AS 'CATEGORIA',
 	T4.stat_name AS 'ESTADO ACTUAL',
 	T3.inci_description AS 'DESCRIPCIÓN'
 FROM 
@@ -20,10 +19,6 @@ ON
  	AFW_STATUS AS T4
  ON
  	T3.inci_status_id = T4.stat_id
- INNER JOIN
- 	ALL_CATEGORY AS T5
- ON
- 	T5.ID = T3.inci_category_id
 WHERE 
 	T1.himo_field_modified = 'REASON' AND 
 	T1.himo_new_value = '16'
