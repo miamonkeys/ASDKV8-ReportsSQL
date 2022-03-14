3b084efe0956069421acca2ac95db3dee880c375
hachavez <hachavez@indracompany.com>
2022-03-13 21:14:58 -0500

    Cambio del formato de fecha para FECHA ESTIMADA

--- SOLIC_SLA.sql
+++ SOLIC_SLA.sql
@@ -1,12 +1,12 @@
 SELECT
 	A.serv_id AS 'ID',
 	A.serv_id_by_project AS 'ID GLOBAL',
-	FORMAT(A.serv_opened_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE APERTURA',
-	FORMAT(A.serv_attention_esti_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE ATENCIÓN ESTIMADA',
-	FORMAT(A.serv_attention_real_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE ATENCIÓN REAL',
-	FORMAT(A.serv_expired_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE SOLUCIÓN ESTIMADA',
-	FORMAT(A.serv_solution_real_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE SOLUCIÓN REAL',
-	FORMAT(A.serv_colsed_date, 'dd/MM/yyyy hh:mm:ss') AS 'FECHA DE CIERRE',
+	FORMAT(A.serv_opened_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE APERTURA',
+	FORMAT(A.serv_attention_esti_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE ATENCIÓN ESTIMADA',
+	FORMAT(A.serv_attention_real_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE ATENCIÓN REAL',
+	FORMAT(A.serv_expired_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE SOLUCIÓN ESTIMADA',
+	FORMAT(A.serv_solution_real_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE SOLUCIÓN REAL',
+	FORMAT(A.serv_colsed_date, 'dd/MM/yyyy HH:mm:ss') AS 'FECHA DE CIERRE',
 	B.NAME AS 'NOMBRE SLA',
 	B.fl_int_times_at3 AS 'TIEMPO DE ATENCIÓN EN MINUTOS',
 	B.fl_int_times_so3 AS 'TIEMPO DE SOLUCIÓN EN MINUTOS'
